3052fa22e35af40824ac42c0e435a2a3f9b1351a
Bryce Sigsbee <btsigsbee@gmail.com>
2018-11-27 19:46:22 -0500

    Started home page

--- App.js
+++ App.js
@@ -7,7 +7,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Card><Text>Meet our Staff</Text></Card>
+        <Card >
+          <Text>Our Staff</Text>
+          <Button title="Meet our staff"></Button>
+        </Card>
         <Card><Text>Applegate Apparel</Text></Card>
       </View>
     );
@@ -21,4 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  redCardStyles: {
+    flex: 1,
+    backgroundColor: '#ef2f24'
+  }
 });
