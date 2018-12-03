a9f149e40fc5d5a4f788b4cbc430b7dcf9636f0b
Bryce Sigsbee <btsigsbee@gmail.com>
2018-12-03 17:13:41 -0500

    Added procedures to procedures page

--- Procedures.js
+++ Procedures.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {} from 'react-native-elements';
+import {Text, ScrollView, StyleSheet, View} from 'react-native';
+import {Card} from 'react-native-elements';
  export default class Procedures extends React.Component{
      render(){
          return(
-            <Text>Hey its me ur boy</Text>
+             <ScrollView >
+             <View style={styles.container}>
+            <Card title="Tooth Whitening">
+                <Text>Teeth Whitening or “bleaching” is a safe, effective procedure to whiten and brighten your teeth and make you look years younger.</Text>
+            </Card>
+            <Card title="Crowns and Bridges">
+                <Text>Crowns and bridges refer to the restoration of teeth using porcelain. Whether covering or capping a tooth (crown) or spanning areas of missing or extracted teeth (bridge), we can meet your needs in this area.</Text>
+            </Card>
+            <Card title="Teeth Straightening">
+                <Text>Straight teeth give you a great smile and self-confidence. Our office can work with you to explain the options we offer in this area.</Text>
+            </Card>
+            <Card title="Porcelain Veneers">
+                <Text>Veneers are a cosmetic solution to change your smile by improving their color, size, or shape. Veneers can be done in 2 simple visits.</Text>
+            </Card>
+
+            <Card title="Tooth Colored Fillings">
+                <Text>Veneers are a cosmetic solution to change your smile by improving their color, size, or shape. Veneers can be done in 2 simple visits.</Text>
+            </Card>
+            </View>
+            </ScrollView>
 
 
          )
      }
  }
+ const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#Ba0121',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },});
