208e5e6553f0dba2a01a69eba694a6dca9c7e77e
daronmiller <dlmiller@bsu.edu>
2019-02-27 17:16:55 -0500

    .

--- App.js
+++ App.js
@@ -17,10 +17,6 @@ class HomeScreen extends React.Component {
   
   render() {
     return (
-<<<<<<< Updated upstream
-=======
-      
->>>>>>> Stashed changes
       <View style={styles.container}>
         <View style={styles.banner}>
           <Image style ={styles.bannerImage} source={require('./assets/ApplegateBanner.jpg')}/>
